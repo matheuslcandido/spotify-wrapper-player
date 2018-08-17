4fe94f46253bf4122e2c8eb51dcad5a853283817
matheuslcandido <matheus.l.candido@gmail.com>
2018-08-17 00:10:13 -0300

    Add AlbumList

--- src/Spotify.js
+++ src/Spotify.js
@@ -1,7 +1,7 @@
 import SpotifyWrapper from 'spotify-wrapper';
 
 const spotify = new SpotifyWrapper({
-  token: 'YOUR_TOKEN_HERE'
+  token: 'BQA87lhi0SrsdAig2ZCmW9k6xm-q_jLpvl0UDz2RhXsVcOUo200drMmYwers5e7U-UVWYllflpGQ7Rog_2fRVg4zQ5NWG06muazLmruZD6WIMtXOzuhmfPnbm8732yQRvovO8sLHo3ohW7RWSBw40LgVW7PbSWN2Nb5Yyu6BF8RGOdd5',
 });
 
 export default spotify;
